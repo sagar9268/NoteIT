91c5458b4baf4a4dbb2c00a5ae9125d76c144584
Sagar <sagar9268@yahoo.com>
2019-11-25 02:13:39 +0530

    Read function added for database and displaying it in a recycler view

--- app/src/main/java/com/sagar/noteit/MainActivity.java
+++ app/src/main/java/com/sagar/noteit/MainActivity.java
@@ -8,32 +8,59 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String ANONYMOUS = "anonymous";
+    public static final String ID = "123";
     public static final int RC_SIGN_IN = 1;
 
     private String mUsername;
+    private String ID_KEY = "id";
+    private String mUserID;
     private Button mButtonAddNote;
     private ActionBar mActionBar;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    //private RecyclerView.Adapter mAdapter;
+
+    private NoteAdapter adapter;
+    private ArrayList<Note> objectList;
 
     //Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mNoteDatabaseReference;
+    private ChildEventListener mChildEventListener;
+
+    FirebaseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +70,41 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mButtonAddNote = findViewById(R.id.buttonAddNote);
+        recyclerView = findViewById(R.id.notelistrec);
 
         mUsername = ANONYMOUS;
+        mUserID = ID;
         mActionBar = getSupportActionBar();
-        
         mActionBar.setDisplayShowTitleEnabled(true);
 
+        objectList = new ArrayList<>();
+
+        objectList.clear();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
         mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        //Log.d(TAG,"User ID: "+user.getUid());
+        mNoteDatabaseReference = mFirebaseDatabase.getReference().child("users").child(user.getUid());//.child("users").child(mUserID).child("notes");
+
+        helper = new FirebaseHelper(mNoteDatabaseReference);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+        objectList = helper.retrieve();
+        adapter = new NoteAdapter(MainActivity.this,objectList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+
+
 
         mButtonAddNote.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent i = new Intent(MainActivity.this,EntryActivity.class);
+                i.putExtra(ID_KEY,mUserID);
                 startActivity(i);
                 finish();
             }
@@ -77,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null){
                     //signed in
-                    onSignedInInitialize(user.getDisplayName());
+                    onSignedInInitialize(user.getDisplayName(),user.getUid());
                 }
                 else{
                     //signed out
@@ -97,6 +147,28 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        /*
+        mNoteDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+                    Note mNote = dataSnapshot1.getValue(Note.class);
+                    objectList.add(mNote);
+                }
+                adapter = new NoteAdapter(MainActivity.this,objectList);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+         */
+
     }
 
     @Override
@@ -128,7 +200,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_show_notes) {
+            adapter.notifyDataSetChanged();
             return true;
         }
         else if(id == R.id.action_sign_out){
@@ -147,7 +220,10 @@ public class MainActivity extends AppCompatActivity {
             mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
         }
         //detachDatabaseReadListener();
-        //mMessageAdapter.clear();
+        //adapter.clear();
+        objectList.clear();
+        adapter.notifyDataSetChanged();
+
     }
 
     @Override
@@ -155,18 +231,71 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onResume();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+
+        adapter.notifyDataSetChanged();
     }
 
-    private void onSignedInInitialize(String username)
+    private void onSignedInInitialize(String username,String userID)
     {
         mUsername = username;
+        mUserID = userID;
+
+        adapter.notifyDataSetChanged();
         //attachDatabaseReadListener();
     }
 
     private void onSignedOutCleanup()
     {
         mUsername = ANONYMOUS;
-        //mMessageAdapter.clear();
+        mUserID = ID;
+        //adapter.clear();
+        objectList.clear();
+        adapter.notifyDataSetChanged();
         //detachDatabaseReadListener();
+    }
+
+    private void attachDatabaseReadListener(){
+        if(mChildEventListener == null){
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Note mNote = dataSnapshot.getValue(Note.class);
+                    objectList.add(mNote);
+
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+            mNoteDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+
+
+
+    }
+
+    private void detachDatabaseReadListener(){
+        if(mChildEventListener != null){
+            mNoteDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
     }
 }
